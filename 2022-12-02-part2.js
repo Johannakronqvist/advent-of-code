6c9225dc053f8ee26fb25fecc1cb63381d67c9b7
Johannakronqvist <78489287+Johannakronqvist@users.noreply.github.com>
2022-12-04 16:47:45 +0100

    day 2 refactored

---
+++ 2022-12-02-part2.js
@@ -0,0 +1,7 @@
+//X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+
+const roundOutcome = {
+  X: 'lose',
+  Y: 'draw',
+  Z: 'win',
+};
--- 2022-12-02.js
+++ 2022-12-02.js
@@ -2,47 +2,21 @@ const fs = require('fs');
 const gameData = fs.readFileSync('rpsData.txt', 'utf-8').split('\r\n');
 
 const gameScores = {
-  winning: { 'A Y': 8, 'B Z': 9, 'C X': 7 },
-  loosing: { 'A Z': 3, 'B X': 1, 'C Y': 2 },
-  draw: { 'A X': 4, 'B Y': 5, 'C Z': 6 },
+  'A Y': 8,
+  'B Z': 9,
+  'C X': 7,
+  'A Z': 3,
+  'B X': 1,
+  'C Y': 2,
+  'A X': 4,
+  'B Y': 5,
+  'C Z': 6,
 };
-
-function getScore(shapes) {
-  switch (shapes) {
-    case 'A Y':
-      totalScore += gameScores.winning['A Y'];
-      break;
-    case 'B Z':
-      totalScore += gameScores.winning['B Z'];
-      break;
-    case 'C X':
-      totalScore += gameScores.winning['C X'];
-      break;
-    case 'A Z':
-      totalScore += gameScores.loosing['A Z'];
-      break;
-    case 'B X':
-      totalScore += gameScores.loosing['B X'];
-      break;
-    case 'C Y':
-      totalScore += gameScores.loosing['C Y'];
-      break;
-    case 'A X':
-      totalScore += gameScores.draw['A X'];
-      break;
-    case 'B Y':
-      totalScore += gameScores.draw['B Y'];
-      break;
-    case 'C Z':
-      totalScore += gameScores.draw['C Z'];
-      break;
-  }
-}
 
 let totalScore = 0;
 
 for (let i = 0; i < gameData.length; i++) {
-  getScore(gameData[i]);
+  totalScore += gameScores[gameData[i]];
 }
 
 console.log('totalScore', totalScore);
