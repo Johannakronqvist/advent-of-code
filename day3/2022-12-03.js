c761f36f89a07916f11c9af2e7dd51987692758f
Johannakronqvist <78489287+Johannakronqvist@users.noreply.github.com>
2022-12-05 09:53:31 +0100

    psudo code

---
+++ day3/2022-12-03.js
@@ -0,0 +1,10 @@
+//Lowercase item types a through z have priorities 1 through 26.
+//Uppercase item types A through Z have priorities 27 through 52.
+
+/**create a variable for characters total value
+ * Loop over the data
+ *  for each string split it in half
+ *  compare the first and second half to find a similar charachter
+ *    loop over the characters of the first string and check if the the second string contains that character
+ *    add that characters value to the variable
+ */
